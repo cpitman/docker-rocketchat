f68d4003e3b3876e31a407efc6c787b3026dcb7c
Chris Pitman <cpitman1@gmail.com>
2018-05-24 14:52:46 -0400

    Adding heap size config

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,9 @@ if [ -z "$INSTANCE_IP" ]; then
     export INSTANCE_IP=`hostname -i`
 fi
 
-exec node main.js
+if [ -z "$MAX_OLD_SPACE_SIZE"]; then
+    echo "Setting MAX_OLD_SPACE_SIZE to default 1536MB"
+    export MAX_OLD_SPACE_SIZE=1536
+fi
+
+exec node --max-old-space-size=$MAX_OLD_SPACE_SIZE main.js
